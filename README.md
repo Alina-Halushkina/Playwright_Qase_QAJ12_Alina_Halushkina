31be18db572d6d873602f8066b76c970ce5018b9
Alina Halushkina <alina.kontekst@gmail.com>
2024-12-16 23:16:36 +0100

    Auth tests

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Playwright_Qase_QAJ12_Alina_Halushkina
+Description of product:
+Qase is a test management tool for organization, tracking, and reporting of manual and automated software testing
+
+Testing checklist:
+1. Authorization
+2. Sign out
+3. Create new project
+4. Create suit
+5. Create test case
+6. Create test plan
+7. Create defect
+8. Create test runs
+9. Create configurations
+10. Create environments
+
+Stack technologies:
+1. TypeScript, JavaScript
+2. Playwright
+3. Allure
+4. Github Actions 
+
+Инструкции по запуску тестов и генерации отчета
+
+1. Create environment variables
+
+```shell
+make project_install
+```
---
+++ tests/authorization.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+
+test('Login with correct email and password', async ({ page }) => {
+  await page.goto('https://app.qase.io/login');
+  await page.getByPlaceholder('Email').fill(process.env.EMAIL);
+  await page.getByPlaceholder('Password').fill(process.env.PASSWORD);
+  await page.getByRole('button', {name: 'Sign'}).click();
+  await expect(page.getByRole('button', { name: 'Create new project' })).toBeVisible();
+});
+
+test('Login with incorrect email and password', async ({ page }) => {
+  await page.goto('https://app.qase.io/login');
+  await page.getByPlaceholder('Email').fill('test@gmail.com');
+    await page.getByPlaceholder('Password').fill('Test1234!');
+    await page.getByRole('button', {name: 'Sign'}).click();
+    await expect(page.getByText('These credentials do not').first()).toBeVisible();
+});
+
+test('Login with incorrect email and simple password', async ({ page }) => {
+  await page.goto('https://app.qase.io/login');
+  await page.getByPlaceholder('Email').fill('test@gmail.com');
+  await page.getByPlaceholder('Password').fill('123');
+  await page.getByRole('button', {name: 'Sign'}).click();
+  await expect(page.getByText('Security notice: The password')).toBeVisible();
+});
