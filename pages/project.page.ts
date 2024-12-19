9d66c02a18ead90a92b219818acd58d84db599ac
Alina Halushkina <alina.kontekst@gmail.com>
2024-12-19 16:06:05 +0100

    Create case test with PO

--- pages/project.page.ts
+++ pages/project.page.ts
@@ -26,7 +26,7 @@ export class ProjectPage extends BasePage {
         return this.page.getByRole('button', { name: 'Suite', exact: true });
     }
 
-    get suiteNameCreate() {
+    get suiteNameField() {
         return this.page.getByPlaceholder('For example: Web Application');
     }
 
@@ -38,6 +38,34 @@ export class ProjectPage extends BasePage {
         return this.page.getByRole('heading', {name: 'Test suite 1'});
     }
 
+    get caseButtonCreate() {
+        return this.page.getByRole('button', {name: 'Case', exact: true});
+    }
+
+    get caseNameField() {
+        return this.page.getByPlaceholder('For example: Authorization');
+    }
+
+    get caseAttachmentButton() {
+        return this.page.getByRole('button', { name: 'Add attachment' });
+    }
+
+    get caseAttachmentBrowse() {
+        return this.page.getByRole('link', { name: 'Browse' });
+    }
+
+    get caseAttachmentFile() {
+        return this.page.getByRole('link', { name: 'disc.png 280.74 KB' });
+    }
+
+    get caseSaveButton() {
+        return this.page.getByRole('button', { name: 'Save', exact: true });
+    }
+
+    get caseName() {
+        return this.page.getByRole('heading', {name: 'Test case 1'});
+    }
+
     async deleteProject() {
         await this.projectSettingsButton.click();
         await this.projectDeleteButton.click();
@@ -46,7 +74,18 @@ export class ProjectPage extends BasePage {
 
     async createSuite(suiteName: string) {
         await this.createSuiteButton.click();
-        await this.suiteNameCreate.fill(suiteName);
+        await this.suiteNameField.fill(suiteName);
         await this.suiteButtonCreate.click();
+    }
+
+    async createCase(caseName: string) {
+        await this.caseButtonCreate.click();
+        await this.caseNameField.fill(caseName);
+        await this.caseAttachmentButton.click();
+        await this.caseAttachmentBrowse.click();
+        await this.caseAttachmentFile.click();
+    }
+    async caseSave() {
+        await this.caseSaveButton.click();
     }
 }
--- tests/createCase.spec.ts
+++ tests/createCase.spec.ts
@@ -1,28 +1,26 @@
 import {test, expect} from '@playwright/test';
-import {beforeEach} from "node:test";
+import {LoginPage} from "../pages/login.page";
+import {HomePage} from "../pages/home.page";
+import {ProjectPage} from "../pages/project.page";
 
-test.beforeEach(
-    async ({page}) => {
-        await page.goto('https://app.qase.io/login');
-        await page.getByPlaceholder('Email').fill(process.env.EMAIL);
-        await page.getByPlaceholder('Password').fill(process.env.PASSWORD);
-        await page.getByRole('button', {name: 'Sign'}).click();
-        await expect(page.getByRole('button', {name: 'Create new project'})).toBeVisible();
-    }
-)
+let loginPage: LoginPage;
+let homePage: HomePage;
+let projectPage: ProjectPage;
+
+test.beforeEach(async ({page}) => {
+    loginPage = new LoginPage(page);
+    homePage = new HomePage(page);
+    projectPage = new ProjectPage(page);
+    await loginPage.goto();
+    await loginPage.login();
+    await expect(homePage.createProjectButton).toBeVisible();
+    await homePage.createProject('Test Project 1', 'TP');
+    await expect(projectPage.projectName).toBeVisible();
+});
 
 test('Create case with attachment', async ({page}) => {
-    await page.getByRole('button', {name: 'Create new project'}).click();
-    await page.getByPlaceholder('For example: Web Application').fill('Test Project 1');
-    await page.getByPlaceholder('For example: WA').fill('TP');
-    await page.getByRole('button', {name: 'Create project'}).click();
-    await expect(page.getByRole('heading', {name: 'Test Project 1'})).toBeVisible();
-    await page.getByRole('button', {name: 'Case', exact: true}).click();
-    await page.getByPlaceholder('For example: Authorization').fill('Test case 1');
-    await page.getByRole('button', { name: 'Add attachment' }).click();
-    await page.getByRole('link', { name: 'Browse' }).click();
-    await page.getByRole('link', { name: 'disc.png 280.74 KB' }).click();
-    await expect(page.getByRole('link', { name: 'disc.png 280.74 KB' })).toBeVisible();
-    await page.getByRole('button', { name: 'Save', exact: true }).click();
-    await expect(page.getByRole('heading', {name: 'Test case 1'})).toBeVisible();
+    await projectPage.createCase('Test case 1');
+    await expect(projectPage.caseAttachmentFile).toBeVisible();
+    await projectPage.caseSave();
+    await expect(projectPage.caseName).toBeVisible();
 });
