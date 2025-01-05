adfc1acde5cd49a876a32031263f092dd0f34519
Alina Halushkina <alina.kontekst@gmail.com>
2025-01-05 19:50:52 +0100

    API test create configuration

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -1,169 +1,169 @@
-import {expect, test} from "@playwright/test";
+import { expect, test } from "@playwright/test";
 import * as allure from "allure-js-commons";
 
-const baseUrl = 'https://api.qase.io/v1';
+const baseUrl = "https://api.qase.io/v1";
 
-test.beforeAll(async ({request}) => {
-    const responsePostProject = await request.post(`${baseUrl}/project`, {
-        data: {
-            "title": "Test project",
-            "code": "TP"
-        },
-        headers: {
-            "Content-Type": "application/json",
-            "Token": process.env.API_TOKEN
-        }
-    })
+test.beforeAll(async ({ request }) => {
+  const responsePostProject = await request.post(`${baseUrl}/project`, {
+    data: {
+      title: "Test project",
+      code: "TP",
+    },
+    headers: {
+      "Content-Type": "application/json",
+      Token: process.env.API_TOKEN,
+    },
+  });
 
-    await expect(responsePostProject.status()).toBe(200);
+  expect(responsePostProject.status()).toBe(200);
 
-    const responseBody = JSON.parse(await responsePostProject.text());
-    expect(responseBody.result.code).toBe("TP");
+  const responseBody = JSON.parse(await responsePostProject.text());
+  expect(responseBody.result.code).toBe("TP");
 });
 
+test.afterAll("Create suite", async ({ request }) => {
+  const responseDeleteProject = await request.delete(`${baseUrl}/project/TP`, {
+    headers: {
+      "Content-Type": "application/json",
+      Token: process.env.API_TOKEN,
+    },
+  });
 
-test.afterAll('Create suite', async ({request}) => {
-    const responseDeleteProject = await request.delete(`${baseUrl}/project/TP`, {
-        headers: {
-            "Content-Type": "application/json",
-            "Token": process.env.API_TOKEN
-        }
-    })
-
-    await expect(responseDeleteProject.status()).toBe(200);
+  expect(responseDeleteProject.status()).toBe(200);
 });
 
+test("Create suite", async ({ request }) => {
+  await allure.epic("API");
+  await allure.feature("Create suite");
+  await allure.severity("Critical");
+  await allure.tag("smoke");
 
-test('Create suite', async ({request}) => {
-    await allure.epic("API");
-    await allure.feature("Create suite");
-    await allure.severity('Critical');
-    await allure.tag("smoke")
+  const responsePostSuite = await request.post(`${baseUrl}/suite/TP`, {
+    data: {
+      title: "Test suite",
+    },
+    headers: {
+      "Content-Type": "application/json",
+      Token: process.env.API_TOKEN,
+    },
+  });
 
-    const responsePostSuite = await request.post(`${baseUrl}/suite/TP`, {
-        data: {
-            "title": "Test suite",
-        },
-        headers: {
-            "Content-Type": "application/json",
-            "Token": process.env.API_TOKEN
-        }
-    })
-
-    await expect(responsePostSuite.status()).toBe(200);
+  expect(responsePostSuite.status()).toBe(200);
 });
 
+test("Create case", async ({ request }) => {
+  await allure.epic("API");
+  await allure.feature("Create case");
+  await allure.severity("Critical");
+  await allure.tag("smoke");
 
-test('Create case', async ({request}) => {
-    await allure.epic("API");
-    await allure.feature("Create case");
-    await allure.severity('Critical');
-    await allure.tag("smoke")
+  const responsePostCase = await request.post(`${baseUrl}/case/TP`, {
+    data: {
+      title: "Test case",
+    },
+    headers: {
+      "Content-Type": "application/json",
+      Token: process.env.API_TOKEN,
+    },
+  });
 
-    const responsePostCase = await request.post(`${baseUrl}/case/TP`, {
-        data: {
-            "title": "Test case",
-        },
-        headers: {
-            "Content-Type": "application/json",
-            "Token": process.env.API_TOKEN
-        }
-    })
-
-    await expect(responsePostCase.status()).toBe(200);
+  expect(responsePostCase.status()).toBe(200);
 });
 
+test("Create plan", async ({ request }) => {
+  await allure.epic("API");
+  await allure.feature("Create plan");
+  await allure.severity("Normal");
 
-test('Create plan', async ({request}) => {
-    await allure.epic("API");
-    await allure.feature("Create plan");
-    await allure.severity('Normal');
+  const responsePostPlan = await request.post(`${baseUrl}/plan/TP`, {
+    data: {
+      title: "Test plan",
+      cases: [1],
+    },
+    headers: {
+      "Content-Type": "application/json",
+      Token: process.env.API_TOKEN,
+    },
+  });
 
-    const responsePostPlan = await request.post(`${baseUrl}/plan/TP`, {
-        data: {
-            "title": "Test plan",
-            "cases": [1]
-        },
-        headers: {
-            "Content-Type": "application/json",
-            "Token": process.env.API_TOKEN
-        }
-    })
-
-    await expect(responsePostPlan.status()).toBe(200);
+  expect(responsePostPlan.status()).toBe(200);
 });
 
+test("Create defect", async ({ request }) => {
+  await allure.epic("API");
+  await allure.feature("Create defect");
+  await allure.severity("Normal");
 
-test('Create defect', async ({request}) => {
-    await allure.epic("API");
-    await allure.feature("Create defect");
-    await allure.severity('Normal');
+  const responsePostDefect = await request.post(`${baseUrl}/defect/TP`, {
+    data: {
+      title: "Test defect",
+      actual_result: "Test defect",
+      severity: 1,
+    },
+    headers: {
+      "Content-Type": "application/json",
+      Token: process.env.API_TOKEN,
+    },
+  });
 
-
-    const responsePostDefect = await request.post(`${baseUrl}/defect/TP`, {
-        data: {
-            "title": "Test defect",
-            "actual_result": "Test defect",
-            "severity": 1,
-        },
-        headers: {
-            "Content-Type": "application/json",
-            "Token": process.env.API_TOKEN
-        }
-    })
-
-    await expect(responsePostDefect.status()).toBe(200);
+  expect(responsePostDefect.status()).toBe(200);
 });
 
+test("Create test run", async ({ request }) => {
+  await allure.epic("API");
+  await allure.feature("Create test run");
+  await allure.severity("Normal");
 
-test('Create test run', async ({request}) => {
-    await allure.epic("API");
-    await allure.feature("Create test run");
-    await allure.severity('Normal');
+  const responsePostRun = await request.post(`${baseUrl}/run/TP`, {
+    data: {
+      title: "Test run",
+      cases: [1],
+    },
+    headers: {
+      "Content-Type": "application/json",
+      Token: process.env.API_TOKEN,
+    },
+  });
 
-    const responsePostRun = await request.post(`${baseUrl}/run/TP`, {
-        data: {
-            "title": "Test run",
-            "cases": [1]
-        },
-        headers: {
-            "Content-Type": "application/json",
-            "Token": process.env.API_TOKEN
-        }
-    })
+  expect(responsePostRun.status()).toBe(200);
+});
 
-    await expect(responsePostRun.status()).toBe(200);
-    });
+/**
+ * @see https://developers.qase.io/reference/create-configuration
+ */
+test("Create configuration", async ({ request }) => {
+  await allure.epic("API");
+  await allure.feature("Create configuration");
+  await allure.severity("Normal");
 
-test('Create configuration', async ({request}) => {
-    await allure.epic("API");
-    await allure.feature("Create configuration");
-    await allure.severity('Normal');
+  const responsePostConfigGroup = await request.post(
+    `${baseUrl}/configuration/TP/group`,
+    {
+      data: {
+        title: "Test configuration group",
+      },
+      headers: {
+        "Content-Type": "application/json",
+        Token: process.env.API_TOKEN,
+      },
+    },
+  );
 
+  expect(responsePostConfigGroup.status()).toBe(200);
 
-    const responsePostConfigGroup = await request.post(`${baseUrl}/configuration/TP/group`, {
-        data: {
-            "title": "Test configuration group",
-        },
-        headers: {
-            "Content-Type": "application/json",
-            "Token": process.env.API_TOKEN
-        }
-    })
+  const responsePostConfiguration = await request.post(
+    `${baseUrl}/configuration/TP`,
+    {
+      data: {
+        title: "Test configuration",
+        group_id: 1,
+      },
+      headers: {
+        "Content-Type": "application/json",
+        Token: process.env.API_TOKEN,
+      },
+    },
+  );
 
-    await expect(responsePostConfigGroup.status()).toBe(200);
-
-
-    const responsePostConfiguration = await request.post(`${baseUrl}/configuration/TP`, {
-        data: {
-            "title": "Test configuration",
-            "group_id": 1,
-        },
-        headers: {
-            "Content-Type": "application/json",
-            "Token": process.env.API_TOKEN
-        }
-    })
-
-    await expect(responsePostConfigGroup.status()).toBe(200);
-    });
+  expect(responsePostConfiguration.status()).toBe(200);
+});
